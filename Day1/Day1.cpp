9da3ccf623cef54c9b15fdeedf401702e77e4c27
Raphal <931674+Bridaroga@users.noreply.github.com>
2024-12-01 16:28:16 -0500

    Función numerosID para guardar los valores del input al vector de pares.

--- Day1/Day1.cpp
+++ Day1/Day1.cpp
@@ -9,6 +9,25 @@ struct par {
     int y;
 };
 
+par numerosID(const std::string& linea) {
+    int tempX { 0 };
+    int tempY { 0 };
+
+    for (std::size_t i = 0; i < linea.size(); i++){
+        std::size_t inicioNumero { linea.find_first_of("0123456789", i) };
+        std::size_t finNumero { linea.find_first_not_of("0123456789", inicioNumero) };
+
+        if (finNumero != std::string::npos) {
+            i = finNumero;
+            tempX = std::stoi(linea.substr(inicioNumero, finNumero));
+        } else {
+            tempY = std::stoi(linea.substr(inicioNumero));
+            break;
+        }
+    }
+    return { tempX, tempY };
+}
+
 // Para organizar los vectores en orden ascendente.
 void organizarVector() {
 
@@ -22,24 +41,8 @@ int main() {
 
     while (!idDocument.eof()) {
         std::getline(idDocument, lineaID);
-        int tempX { 0 };
-        int tempY { 0 };
-        // auto tempID = numerosID(lineaID);
-        // vectorLista1.push_back(std::get<0>(tempID));
-
-        for (std::size_t i = 0; i < lineaID.size(); i++){
-            std::size_t inicioNumero { lineaID.find_first_of("0123456789", i) };
-            std::size_t finNumero { lineaID.find_first_not_of("0123456789", inicioNumero) };
-
-            if (finNumero != std::string::npos) {
-                i = finNumero;
-                tempX = std::stoi(lineaID.substr(inicioNumero, finNumero));
-            } else {
-                tempY = std::stoi(lineaID.substr(inicioNumero));
-                break;
-            }
-        }
-        vectorLista.push_back( {tempX, tempY} );
+        
+        vectorLista.push_back( numerosID(lineaID) );
         std::cout << vectorLista[contador].x << "  " << vectorLista[contador].y << '\n';
         contador++;
     }
