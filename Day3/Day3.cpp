dd59bd2e158cfbdd89302b56eb6fc6cfeee00952
Raphal <931674+Bridaroga@users.noreply.github.com>
2025-01-29 21:16:15 -0500

    Day3 terminado.

--- Day3/Day3.cpp
+++ Day3/Day3.cpp
@@ -2,32 +2,47 @@
 #include <string>
 #include <fstream>
 #include <regex>
+#include <vector>
 
+int multiplicacion(const std::string& linea) {
+    std::vector<int> valores {};
+    std::string numero { "" };
 
+    for (std::size_t i = 0; i < linea.size(); i++) {
+        std::size_t inicioNumero { linea.find_first_of("0123456789", i) };
+        std::size_t tempIndex = linea.find_first_of(",)", inicioNumero);
+        
+        i = tempIndex;
+        numero = linea.substr(inicioNumero, tempIndex);
+        valores.push_back(std::stoi(numero));
+
+        if (linea[i] == ')') {
+            break;
+        }
+    }
+
+    return valores[0] * valores[1];
+}
 
 int main() {
     std::ifstream memoriaDocument("Memoria.txt");
     std::string lineaMemoria { "" };
     std::regex reg("(mul\\(\\d+,\\d+\\))");
-    // std::smatch matchString;
+    int sumaMultiplicacion { 0 };
 
     while (!memoriaDocument.eof()) {
         std::getline(memoriaDocument, lineaMemoria);
-        
-        // Usar for() para buscar todas las coincidencias.
-        // std::regex_search(lineaMemoria, matchString, reg);
 
         auto inicioLinea = std::sregex_iterator(lineaMemoria.begin(), lineaMemoria.end(), reg);
         auto finLinea = std::sregex_iterator();
 
-        std::cout << "Se encontraron " << std::distance(inicioLinea, finLinea) << " coincidencias.\n";
-
         for (std::sregex_iterator i = inicioLinea; i != finLinea; i++) {
             std::smatch match = *i;
             std::string matchStr = match.str();
-            std::cout << matchStr << '\n';
-        }
 
-        std::cout << '\n';
+            sumaMultiplicacion += multiplicacion(matchStr);
+        }
     }
+
+    std::cout << sumaMultiplicacion << '\n';
 }
