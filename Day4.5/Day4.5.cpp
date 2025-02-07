c6ff8dd86dcc1077ada1a865e4a7c377249416b1
Raphal <931674+Bridaroga@users.noreply.github.com>
2025-02-06 22:36:33 -0500

    Buscar X-MAS, arriba-derecha y abajo-derecha.

--- Day4.5/Day4.5.cpp
+++ Day4.5/Day4.5.cpp
@@ -26,102 +26,22 @@ int main() {
     for (std::size_t i = 1; i < crossword.size() - BORDE; i++) {
         for (std::size_t j = 1; j < crossword[i].size() - BORDE; j++) {
             if (mas[POSICION_A] == crossword[i][j]) {
-                // 1) Arriba.
-                if (i >= BORDE) {
-                    char charPalabra[] = { crossword[i][j],
-                                           crossword[i - 1][j],
-                                           crossword[i - 2][j],
-                                           crossword[i - 3][j],
-                                           '\0' };
-                    std::string palabra { charPalabra };
-                    if (mas == palabra) {
-                        conteoXMAS++;
-                    }
+                // 1) Arriba-derecha, Abajo-derecha.
+                char charPalabra[] = { crossword[i + 1][j - 1],
+                                       crossword[i][j],
+                                       crossword[i - 1][j + 1],
+                                       '\0' };
+                char charPalabra2[] = { crossword[i - 1][j - 1],
+                                        crossword[i][j],
+                                        crossword[i + 1][j + 1],
+                                        '\0' };
+                std::string palabra { charPalabra };
+                std::string palabra2 { charPalabra2 };
+                if (mas == palabra && mas == palabra2) {
+                    conteoXMAS++;
                 }
-                // 2) Arriba-derecha.
-                if (i >= BORDE && j < crossword[i].size() - BORDE) {
-                    char charPalabra[] = { crossword[i][j],
-                                           crossword[i - 1][j + 1],
-                                           crossword[i - 2][j + 2],
-                                           crossword[i - 3][j + 3],
-                                           '\0' };
-                    std::string palabra { charPalabra };
-                    if (mas == palabra) {
-                        conteoXMAS++;
-                    }
-                }
-                // 3) Derecha.
-                if (j < crossword[i].size() - BORDE) {
-                    char charPalabra[] = { crossword[i][j],
-                                           crossword[i][j + 1],
-                                           crossword[i][j + 2],
-                                           crossword[i][j + 3],
-                                           '\0' };
-                    std::string palabra { charPalabra };
-                    if (mas == palabra) {
-                        conteoXMAS++;
-                    }
-                }
-                // 4) Abajo-derecha.
-                if (j < crossword[i].size() - BORDE && i < crossword.size() - BORDE) {
-                    char charPalabra[] = { crossword[i][j],
-                                           crossword[i + 1][j + 1],
-                                           crossword[i + 2][j + 2],
-                                           crossword[i + 3][j + 3],
-                                           '\0' };
-                    std::string palabra { charPalabra };
-                    if (mas == palabra) {
-                        conteoXMAS++;
-                    }
-                }
-                // 5) Abajo.
-                if (i < crossword.size() - BORDE) {
-                    char charPalabra[] = { crossword[i][j],
-                                           crossword[i + 1][j],
-                                           crossword[i + 2][j],
-                                           crossword[i + 3][j],
-                                           '\0' };
-                    std::string palabra { charPalabra };
-                    if (mas == palabra) {
-                        conteoXMAS++;
-                    }
-                }
-                // 6) Abajo-izquierda.
-                if (i < crossword.size() - BORDE && j >= BORDE) {
-                    char charPalabra[] = { crossword[i][j],
-                                           crossword[i + 1][j - 1],
-                                           crossword[i + 2][j - 2],
-                                           crossword[i + 3][j - 3],
-                                           '\0' };
-                    std::string palabra { charPalabra };
-                    if (mas == palabra) {
-                        conteoXMAS++;
-                    }
-                }
-                // 7) Izquierda.
-                if (j >= BORDE) {
-                    char charPalabra[] = { crossword[i][j],
-                                           crossword[i][j - 1],
-                                           crossword[i][j - 2],
-                                           crossword[i][j - 3],
-                                           '\0' };
-                    std::string palabra { charPalabra };
-                    if (mas == palabra) {
-                        conteoXMAS++;
-                    }
-                }
-                // 8) Arriba-izquierda.
-                if (j >= BORDE && i >= BORDE) {
-                    char charPalabra[] = { crossword[i][j],
-                                           crossword[i - 1][j - 1],
-                                           crossword[i - 2][j - 2],
-                                           crossword[i - 3][j - 3],
-                                           '\0' };
-                    std::string palabra { charPalabra };
-                    if (mas == palabra) {
-                        conteoXMAS++;
-                    }
-                }
+
+                // 2) Arriba-derecha, Arriba-izquierda.
             }
         }
     }
