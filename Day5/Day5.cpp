26e0f64344545582813b2e4d4920a16c4fbb1e9b
Raphal <931674+Bridaroga@users.noreply.github.com>
2025-02-09 14:21:59 -0500

    Cosas que hacer.

---
+++ Day5/Day5.cpp
@@ -0,0 +1,4 @@
+/*  Guardar las reglas en un vector<tuple<int,int>>, ¿o en un árbol?.
+    Guardar las impresiones en un vector<int>.
+    Recorrer las impresiones con todas las reglas y verificar que ande bonito.
+*/
