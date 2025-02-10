cb1eb9117e745f03e5814a6b1560ff508a81dbff
Raphal <931674+Bridaroga@users.noreply.github.com>
2025-02-09 19:57:59 -0500

    Crear BST para guardar las reglas.

--- Day5/Day5.cpp
+++ Day5/Day5.cpp
@@ -2,3 +2,57 @@
     Guardar las impresiones en un vector<int>.
     Recorrer las impresiones con todas las reglas y verificar que ande bonito.
 */
+
+#include <iostream>
+#include <fstream>
+
+// Nodo con solamente apuntadores a la izquierda y derecha.
+struct nodo {
+    int pagIzquierda;
+    int pagDerecha;
+    struct nodo* izquierda;
+    struct nodo* derecha;
+};
+
+// Crea un nuevo nodo.
+nodo* nuevoNodo(int pagIzquierda, int pagDerecha) {
+    struct nodo* temporal = new struct nodo;
+    temporal->pagIzquierda = pagIzquierda;
+    temporal->pagDerecha = pagDerecha;
+    temporal->izquierda = NULL;
+    temporal->derecha = NULL;
+
+    return temporal;
+}
+
+// Inserta un nuevo nodo.
+nodo* insertar(struct nodo* nodo, int pagIzquierda, int pagDerecha) {
+    if (nodo == NULL) {
+        return nuevoNodo(pagIzquierda, pagDerecha);
+    }
+
+    if (pagDerecha < nodo->pagDerecha) {
+        nodo->izquierda = insertar(nodo->izquierda, pagIzquierda, pagDerecha);
+    }
+    else {
+        nodo->derecha = insertar(nodo->derecha, pagIzquierda, pagDerecha);
+    }
+
+    return nodo;
+}
+
+void printEnOrden(struct nodo* nodo) {
+    if (nodo == NULL) {
+        return;
+    }
+
+    printEnOrden(nodo->izquierda);
+
+    std::cout << "Izquierda: " << nodo->pagIzquierda << "    Apuesta: " << nodo->pagDerecha << '\n';
+    
+    printEnOrden(nodo->derecha);
+}
+
+int main() {
+    
+}
